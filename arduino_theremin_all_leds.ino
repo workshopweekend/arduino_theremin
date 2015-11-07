9d9ab43d4baae580e7c18c5d3ec7b89fa3d9a6ca
Smitha <smiyai25@gmail.com>
2015-11-07 14:31:56 -0800

    Fix LED init in example 2

--- arduino_theremin_all_leds.ino
+++ arduino_theremin_all_leds.ino
@@ -19,7 +19,13 @@ void setup()
 {
   pinMode (trigger, OUTPUT);
   pinMode(echo, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
   digitalWrite(trigger, LOW);
   Serial.begin(9600);
 }
